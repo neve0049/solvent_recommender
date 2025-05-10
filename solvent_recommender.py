4e44b2a0b1070fd3e8e3ba576f3f8363845154a4
neve0049 <neve.clement@hotmail.com>
2025-05-10 15:59:05 +0200

    Update solvent_recommender.py

--- solvent_recommender.py
+++ solvent_recommender.py
@@ -265,6 +265,7 @@ def main():
         if mol:
             st.subheader("Structure moléculaire")
             img = Draw.MolToImage(mol, size=(400, 400))
+            img = img.resize((200, 200))
             buf = BytesIO()
             img.save(buf, format="PNG")
             st.image(buf.getvalue(), use_container_width=True)
