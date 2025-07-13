0b21c7b51adf549ed7edea9378cecac272b750a1
neve0049 <neve.clement@hotmail.com>
2025-07-13 15:58:28 +0200

    Update solvent_recommender.py

--- solvent_recommender.py
+++ solvent_recommender.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
+import matplotlib.pyplot as plt
 from io import BytesIO
 import webbrowser
 import base64
